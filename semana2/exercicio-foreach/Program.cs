bc5b7e8c209fd6c7cbb786205d3fa6a57ded4425
ValberF <valberfrsantos@gmail.com>
2023-11-14 10:07:21 -0300

    class exercices

--- semana2/exercicio-foreach/Program.cs
+++ semana2/exercicio-foreach/Program.cs
@@ -1,7 +1,47 @@
-﻿#region foreach
+﻿/*#region foreach
 string[] colecao = {"Item1", "Item2", "Item3", "Item4"};
 
 foreach (string item in colecao) {
     Console.WriteLine(item);
 }
+#endregion
+
+#region ReadLine exemple
+
+Console.WriteLine("Please enter your name: ");
+string? name = Console.ReadLine();
+Console.WriteLine($"Hello, {name}");
+
+#endregion
+
+#region Print 3 and 4 divisors
+
+for (int i = 0; i <= 30; i++) {
+    if(i % 3 == 0 || i % 4 == 0) {
+        Console.WriteLine(i);
+    }
+}
+
+#endregion */
+
+#region fibonacci
+
+int previous = 0, actual = 1, fib = 0;
+
+Console.WriteLine(previous);
+Console.WriteLine(actual);
+while (fib < 100) {
+    fib = previous + actual;
+    Console.WriteLine(fib);
+    previous = actual;
+    actual = fib;
+}
+
+#endregion
+
+
+#region table
+
+
+
 #endregion
