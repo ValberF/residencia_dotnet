f00e308d9a633055d49de0c488a6ea87b6c9b860
ValberF <valberfrsantos@gmail.com>
2023-11-22 09:30:37 -0300

    exercicio linq

---
+++ semana3/exercicios_linq/Program.cs
@@ -0,0 +1,5 @@
+﻿string[] people = {"Valber", "Joao", "Zaca", "Amaral"};
+
+char letter = 'A';
+
+Console.WriteLine($"Pessoas com a ocorrência da letra '{letter}': {string.Join(", ", people.Where(x => x.ToLower().Contains(Char.ToLower(letter))))}");
