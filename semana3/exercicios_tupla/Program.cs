cbf81500e949b60917b3e81a9c2bbc830942fa41
ValberF <valberfrsantos@gmail.com>
2023-11-22 09:11:59 -0300

    exercicio tupla

---
+++ semana3/exercicios_tupla/Program.cs
@@ -0,0 +1,8 @@
+﻿var tuple3 = (id: 10, name: "Valber", born: new DateTime(1996, 6, 18));
+
+Console.WriteLine($"Tuple 3: {tuple3.id}, {tuple3.name}, {tuple3.born}");
+
+List<(int id, string name, DateTime born)> list = new();
+list.Add(tuple3);
+list.Add((11, "Nicole", new DateTime(2001, 9, 12)));
+list.ForEach(x => Console.WriteLine($"Tuple 4: {x.id}, {x.name}, {x.born.ToShortDateString()}"));
