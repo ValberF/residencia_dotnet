419b1715a58bf30342734f7ddabc53aefc7a569e
ValberF <valberfrsantos@gmail.com>
2023-11-22 12:26:21 -0300

    exercicio linq

---
+++ semana3/exercicios_linq_idade/Program.cs
@@ -0,0 +1,34 @@
+﻿var students = new List<Student>{
+   new Student(1, "Helder Almeida", "123456789", new DateTime(1987, 9, 24), new List<string>{"123456789", "73988342986"}),
+   new Student(2, "Nicole Almeida", "987654321", new DateTime(2019, 1, 12), new List<string>{"123456789", "73987654321"}),
+   new Student(3, "Gilvana Rocha", "123456789", new DateTime(1984, 11, 24), new List<string>{"123456789", "77988237086"})
+};
+
+var any = students.Any();
+var anyHelder = students.Any(x => x.FullName.Contains("Helder"));
+//var single = students.Single(x => x.Id == 10);
+//throw exception
+var singleOrDefault = students.SingleOrDefault(x => x.Id == 10);
+
+var select = students.Select(x => x.PhoneNumbers);
+var selectMany = students.SelectMany(x => x.PhoneNumbers);
+
+var legalAge = students.Where(x => x.BirthDate <= DateTime.Today.AddYears(-18)).Select(x => x.FullName);
+Console.WriteLine($"Legal age people: {string.Join(", ", legalAge)}");
+
+ public class Student{
+   public Student(int id, string fullName, string document, DateTime birthDate, List<string> phoneNumbers)
+   {
+      Id = id;
+      FullName = fullName;
+      Document = document;
+      BirthDate = birthDate;
+      PhoneNumbers = new List<string>(phoneNumbers);
+   }
+
+   public int Id { get; set; }
+   public string FullName { get; set; }
+   public string Document { get; set; }
+   public DateTime BirthDate { get; set; }
+   public List<string> PhoneNumbers { get; set; }
+}
