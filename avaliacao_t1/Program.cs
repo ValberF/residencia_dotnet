2eaecc6029de03ab1408cfa916b922eca798c157
ValberF <valberfrsantos@gmail.com>
2023-11-29 12:10:51 -0300

    Testando criação de médicos e pacientes 4

--- avaliacao_t1/Program.cs
+++ avaliacao_t1/Program.cs
@@ -14,19 +14,24 @@ doctorList.Add(doctor3);
 List<string> sintomas1 = new List<string> { "Febre", "Tosse" };
 List<string> sintomas2 = new List<string> { "Coceira", "pigarro" };
 List<string> sintomas3 = new List<string> { "Dor de cabeça", "incomodo nas costas" };
+List<string> sintomas4 = new List<string> {};
 
 Patient patient1 = new Patient("Valber", new DateTime(1996, 6, 19), "07109001555", "masculino", sintomas1);
 Patient patient2 = new Patient("Joao", new DateTime(1999, 7, 22), "12345678912", "masculino", sintomas3);
 Patient patient3 = new Patient("Zaca", new DateTime(1997, 6, 19), "98765432121", "feminino", sintomas2);
+Patient patient4 = new Patient("Joana", new DateTime(1997, 6, 19), "11223344551", "feminino", sintomas4);
 
 List<Patient> patientList = new List<Patient>();
 patientList.Add(patient1);
 patientList.Add(patient2);
 patientList.Add(patient3);
+patientList.Add(patient4);
 
 var report1 = doctorList.Where(doctor => doctor.Age >= 30 && doctor.Age <= 45);
 var report2 = patientList.Where(patient => patient.Age >= 25 && patient.Age <= 30);
-
+var report3 = patientList.Where(patient => patient.Gender == "masculino" );
+var report4 = patientList.OrderBy(patient => patient.Name );
+var report5 = patientList.Where(patient => patient.Symptoms.Count > 0);
 
 Console.WriteLine($"Relatório 1: Médicos com idade entre {30} e {45} anos");
 
@@ -39,19 +44,39 @@ Console.WriteLine($"Relatório 2: Pacientes com idade entre {25} e {30} anos");
 
 foreach (var patient in report2)
 {
-    Console.WriteLine($"Nome do médico: {patient.Name} - Idade: {patient.Age} - CPF: {patient.Cpf}");
+    Console.WriteLine($"Nome do paciente: {patient.Name} - Idade: {patient.Age} - CPF: {patient.Cpf}");
+    Console.WriteLine($"Sintomas");
+    patient.Symptoms.ForEach(s => Console.WriteLine(s));
+    Console.WriteLine($"\n");
 }
 
-/* Console.WriteLine($"Relatório 1: Médicos com idade entre {30} e {45} anos");
+Console.WriteLine($"Relatório 3: Pacientes do sexo masculino");
 
-foreach (var doctor in report2)
+foreach (var patient in report3)
 {
-    Console.WriteLine($"Nome do médico: {doctor.Name} - Idade: {doctor.Age} - CPF: {doctor.Cpf} - CRM: {doctor.Crm}");
+    Console.WriteLine($"Nome do paciente: {patient.Name} - Idade: {patient.Age} - CPF: {patient.Cpf}");
+    Console.WriteLine($"Sintomas");
+    patient.Symptoms.ForEach(s => Console.WriteLine(s));
+    Console.WriteLine($"\n");
 }
 
-Console.WriteLine($"Relatório 1: Médicos com idade entre {30} e {45} anos");
 
-foreach (var doctor in report2)
+Console.WriteLine($"Relatório 4: Pacientes em ordem alfabética");
+
+foreach (var patient in report4)
 {
-    Console.WriteLine($"Nome do médico: {doctor.Name} - Idade: {doctor.Age} - CPF: {doctor.Cpf} - CRM: {doctor.Crm}");
-} */
+    Console.WriteLine($"Nome do paciente: {patient.Name} - Idade: {patient.Age} - CPF: {patient.Cpf}");
+    Console.WriteLine($"Sintomas");
+    patient.Symptoms.ForEach(s => Console.WriteLine(s));
+    Console.WriteLine($"\n");
+}
+
+Console.WriteLine($"Relatório 5: Pacientes que informaram sintomas");
+
+foreach (var patient in report5)
+{
+    Console.WriteLine($"Nome do paciente: {patient.Name} - Idade: {patient.Age} - CPF: {patient.Cpf}");
+    Console.WriteLine($"Sintomas");
+    patient.Symptoms.ForEach(s => Console.WriteLine(s));
+    Console.WriteLine($"\n");
+}
